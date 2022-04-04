00eca731b8a89a19deb70009265c84e4a3beb8dc
healym3 <79485702+healym3@users.noreply.github.com>
2022-04-04 16:04:54 -0500

    Errors part 1

--- app/src/main/java/healym3/bitmaptobytes/MainActivity.java
+++ app/src/main/java/healym3/bitmaptobytes/MainActivity.java
@@ -2,6 +2,8 @@ package healym3.bitmaptobytes;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.res.AssetFileDescriptor;
+import android.content.res.AssetManager;
 import android.os.Bundle;
 
 import android.graphics.Bitmap;
@@ -14,8 +16,23 @@ import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.Toast;
 import java.io.ByteArrayOutputStream;
+import java.io.DataInputStream;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
 import java.io.IOException;
 import java.io.InputStream;
+import java.io.OutputStream;
+import java.security.InvalidAlgorithmParameterException;
+import java.security.InvalidKeyException;
+import java.security.NoSuchAlgorithmException;
+
+import javax.crypto.BadPaddingException;
+import javax.crypto.IllegalBlockSizeException;
+import javax.crypto.NoSuchPaddingException;
+import javax.crypto.SecretKey;
+import javax.crypto.spec.IvParameterSpec;
+
 public class MainActivity extends AppCompatActivity {
     Button button;
     ImageView ivSource, ivCompressed;
@@ -26,33 +43,104 @@ public class MainActivity extends AppCompatActivity {
         button = findViewById(R.id.button);
         ivCompressed = findViewById(R.id.ivCompressed);
         ivSource = findViewById(R.id.ivSource);
+        try {
+            givenFile_whenEncrypt_thenSuccess();
+        } catch (NoSuchAlgorithmException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        } catch (IllegalBlockSizeException e) {
+            e.printStackTrace();
+        } catch (InvalidKeyException e) {
+            e.printStackTrace();
+        } catch (BadPaddingException e) {
+            e.printStackTrace();
+        } catch (InvalidAlgorithmParameterException e) {
+            e.printStackTrace();
+        } catch (NoSuchPaddingException e) {
+            e.printStackTrace();
+        }
         button.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 try {
-                    InputStream inputStream = getAssets().open("image.bmp");
-                    Bitmap bitmap = BitmapFactory.decodeStream(inputStream);
-                    ivSource.setImageBitmap(bitmap);
-                    ByteArrayOutputStream stream = new ByteArrayOutputStream();
-                    bitmap.compress(Bitmap.CompressFormat.JPEG,80,stream);
-                    byte[] byteArray = stream.toByteArray();
-                    Bitmap compressedBitmap = BitmapFactory.decodeByteArray(byteArray,0,byteArray.length);
-                    ivCompressed.setImageBitmap(compressedBitmap);
-                    Toast.makeText(getApplicationContext(),
-                            "ByteArray created..",
-                            Toast.LENGTH_SHORT).show();
-                    StringBuilder stringBuilder = new StringBuilder();
-                    for (byte b: byteArray
+                    DataInputStream dataInputStream = new DataInputStream(getAssets().open("test2.bmp"));
+                    byte[] image = new byte[60];
+                    dataInputStream.readFully(image, 0, 54);
+                    StringBuilder sb = new StringBuilder();
+                    for (byte b: image
                          ) {
-                        stringBuilder.append(String.valueOf(b) + ", ");
+                        sb.append(String.valueOf(b) + ", ");
                         //
                     }
+                    Log.d("imageBytes", "onClick: image bytes " + sb.toString());
+                    dataInputStream.close();
 
-                    Log.d("byteArray", "onClick: " + stringBuilder.toString());
+                    //InputStream inputStream = getAssets().open("test.bmp");
+//                    Bitmap bitmap = BitmapFactory.decodeStream(inputStream);
+//                    ivSource.setImageBitmap(bitmap);
+//                    ByteArrayOutputStream stream = new ByteArrayOutputStream();
+//                    bitmap.compress(Bitmap.CompressFormat.JPEG,80,stream);
+//                    byte[] byteArray = stream.toByteArray();
+//                    Bitmap compressedBitmap = BitmapFactory.decodeByteArray(byteArray,0,byteArray.length);
+//                    ivCompressed.setImageBitmap(compressedBitmap);
+//                    Toast.makeText(getApplicationContext(),
+//                            "ByteArray created..",
+//                            Toast.LENGTH_SHORT).show();
+//                    StringBuilder stringBuilder = new StringBuilder();
+//                    for (byte b: byteArray
+//                         ) {
+//                        stringBuilder.append(String.valueOf(b) + ", ");
+//                        //
+//                    }
+//
+//                    Log.d("byteArray", "onClick: " + stringBuilder.toString());
                 } catch (IOException e) {
                     e.printStackTrace();
                 }
             }
         });
+    }
+
+    void givenFile_whenEncrypt_thenSuccess()
+            throws NoSuchAlgorithmException, IOException, IllegalBlockSizeException,
+            InvalidKeyException, BadPaddingException, InvalidAlgorithmParameterException,
+            NoSuchPaddingException {
+
+        SecretKey key = AES.generateKey(128);
+        String algorithm = "AES/CBC/PKCS5Padding";
+        IvParameterSpec ivParameterSpec = AES.generateIv();
+        AssetManager assetManager = getAssets();
+        AssetFileDescriptor fileDescriptor = assetManager.openFd("test2.bmp");
+        FileInputStream stream = fileDescriptor.createInputStream();
+        //File inputFile = new DataInputStream(getAssets().open("test2.bmp"));
+        File encryptedFile = new File("classpath:baeldung.encrypted");
+        //File decryptedFile = new File("document.decrypted");
+        AES.encryptFile(algorithm, key, ivParameterSpec, stream, encryptedFile);
+
+
+    }
+
+    private File createFileFromInputStream(InputStream inputStream) {
+
+        try{
+            File f = new File("newFilePath");
+            OutputStream outputStream = new FileOutputStream(f);
+            byte buffer[] = new byte[1024];
+            int length = 0;
+
+            while((length=inputStream.read(buffer)) > 0) {
+                outputStream.write(buffer,0,length);
+            }
+
+            outputStream.close();
+            inputStream.close();
+
+            return f;
+        }catch (IOException e) {
+            //Logging exception
+        }
+
+        return null;
     }
 }
